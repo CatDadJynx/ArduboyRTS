e41fa3615923bf1bf579afc75bcd440375fe8395
CatDadJynx <Andrew.crawford.440@gmail.com>
2019-05-11 15:02:02 -0400

    Update Resources.h

--- Resources.h
+++ Resources.h
@@ -1,34 +1,36 @@
-#include "Sprites.h"
+#include "GameSprites.h"
 #include "camera.h"
 
-uint16_t rockX[50];
-uint16_t rockY[50];
+constexpr uint8_t resourceMax = 30;
+
+uint16_t rockX[resourceMax];
+uint16_t rockY[resourceMax];
 
 void populateRock(){
-  for (int x = 0; x < 30; x++) {
+  for (int x = 0; x < resourceMax; x++) {
     rockX[x] = random(0, 252);
     rockY[x] = random(0, 252);
   }
 }
 
 void drawRock() {
-  for (uint8_t x = 0; x < 30; x++) {
+  for (uint8_t x = 0; x < resourceMax; x++) {
     Sprites::drawSelfMasked(rockX[x] + camera.x, rockY[x] + camera.y, rock, 0);
   }
 }
 
-uint16_t treeX[50];
-uint16_t treeY[50];
+uint16_t treeX[resourceMax];
+uint16_t treeY[resourceMax];
 
 void populateTree(){
-  for (int x = 0; x < 30; x++) {
+  for (int x = 0; x < resourceMax; x++) {
     treeX[x] = random(0, 252);
     treeY[x] = random(0, 252);
   }
 }
 
 void drawTree() {
-  for (uint8_t x = 0; x < 30; x++) {
+  for (uint8_t x = 0; x < resourceMax; x++) {
     Sprites::drawSelfMasked(treeX[x] + camera.x, treeY[x] + camera.y, tree, 0);
   }
 }
