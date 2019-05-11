a3e388ce23f06ec26872319828826adfe34c2217
CatDadJynx <Andrew.crawford.440@gmail.com>
2019-05-11 15:01:13 -0400

    Update units.h
    Made units selectable

--- units.h
+++ units.h
@@ -1,21 +1,33 @@
+
 struct Timer {
   unsigned long currentMillis;
   unsigned long previousMillis = 0;
 };
 
-Point person;
 Timer timer;
+Point personPos;
 
+constexpr uint8_t personMax = 50;
 uint8_t personFrame = 0;
-uint8_t walk;
+uint8_t walk; //turn into enum class
+uint16_t personX[personMax];
+uint16_t personY[personMax];
+uint8_t personCount;
+uint8_t personSelect;
+uint8_t personState[personMax];
 
 void populatePerson() {
-  person.x = playerCursor.x;
-  person.y = playerCursor.y;
+  personX[0] = playerCursor.x;
+  personY[0] = playerCursor.y;
+  personCount += 1;
 }
 
 void drawPerson() {
-  Sprites::drawSelfMasked(person.x + camera.x, person.y + camera.y, personSprite, personFrame);
+  for (uint8_t x = 0; x < personCount; x++) {
+    Sprites::drawSelfMasked(personX[x] + camera.x, personY[x] + camera.y, personSprite, personFrame);
+    personPos.x = personX[x];
+    personPos.y = personY[x];
+  }
 }
 
 void animatePerson() {
@@ -27,26 +39,64 @@ void animatePerson() {
 }
 
 void personWalk() {
-  if (timer.currentMillis - timer.previousMillis >= 500) {
-    walk = random(0, 4);
-    switch ( walk ) {
-      case 0:
-        ++person.x;
-        animatePerson();
-        break;
-      case 1:
-        --person.x;
-        animatePerson();
-        break;
-      case 2:
-        ++person.y;
-        animatePerson();
-        break;
-      case 3:
-        --person.y;
-        animatePerson();
-        break;
+  for (uint8_t x = 0; x < personCount; x++) {
+    if (timer.currentMillis - timer.previousMillis >= 500) {
+      walk = random(0, 4);
+      switch ( walk ) {
+        case 0:
+          ++personX[x];
+          animatePerson();
+          break;
+        case 1:
+          --personX[x];
+          animatePerson();
+          break;
+        case 2:
+          ++personY[x];
+          animatePerson();
+          break;
+        case 3:
+          --personY[x];
+          animatePerson();
+          break;
+      }
+      timer.previousMillis = timer.currentMillis;
     }
-    timer.previousMillis = timer.currentMillis;
   }
+}
+
+void personSelection() {
+  for (uint8_t x = 0; x < personCount; x++) {
+    if (personState[x] == 0 && arduboy.collide(personPos, rectangle)) {
+      personState[x] = 1;
+      if (personState[x] = 1){
+        ++personSelect;  
+      }
+    }
+    if (arduboy.pressed(B_BUTTON)){
+      personState[x] = 0;
+      personSelect = 0;
+    }
+  }
+}
+
+void drawDebugInfo()
+{
+  arduboy.setCursor(0, 0);
+  arduboy.print(playerCursor.x);
+
+  arduboy.setCursor(20, 0);
+  arduboy.print(playerCursor.y);
+
+  arduboy.setCursor(0, 20);
+  arduboy.print(rectangle.x);
+
+  arduboy.setCursor(20, 20);
+  arduboy.print(rectangle.y);
+
+  arduboy.setCursor(0, 40);
+  arduboy.print(personX[0]);
+
+  arduboy.setCursor(20, 40);
+  arduboy.print(personSelect);
 }
