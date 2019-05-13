14acb2b9a00d0939b7d0ba29dd0de60779b0a0e3
CatDadJynx <Andrew.crawford.440@gmail.com>
2019-05-12 22:44:21 -0400

    Update units.h

--- units.h
+++ units.h
@@ -1,49 +1,127 @@
-#include "GameSprites.h"
-#include "camera.h"
 
-enum class ResourceState : uint8_t
-{
-  Active,
-  Inactive
+struct Timer {
+  unsigned long currentMillis;
+  unsigned long previousMillis = 0;
 };
 
-struct Resource
+Timer timer;
+Point personPos;
+constexpr uint8_t personMax = 40;
+
+uint8_t personFrame = 0;
+uint8_t walk; //turn into enum class
+
+enum class PersonState : uint8_t
+{
+  notSelected,
+  selected
+};
+
+struct Person
 {
   Point position;
-  ResourceState state;
+  PersonState state;
 };
 
-// This will take the same amount of RAM
-constexpr uint8_t resourceMax = 30;
+Person people[personMax]
+// The first set of braces is 'all the people'
+{
+  // This set of braces is people[0]
+  {
+    // This is people[0].position
+    { cursorGlobalPos.x, cursorGlobalPos.y },
+    // This is people[0].state
+    PersonState::notSelected
+  }
+  // The rest of your people can be left out and they'll just be 'zero initialised'
+};
 
-Resource rock[resourceMax];
-Resource tree[resourceMax];
+// You start off with 1 person initialised
+uint8_t personCount = 1;
+uint8_t personSelect;
 
-void populateResources(){
-  for(uint8_t i = 0; i < resourceMax; i++){
-    rock[i].position.x = random(0,250);
-    rock[i].position.y = random(0,250);
-    rock[i].state = ResourceState::Active;
-    tree[i].position.x = random(0,250);
-    tree[i].position.y = random(0,250);
-    tree[i].state = ResourceState::Active;
+void drawPerson() {
+  for (uint8_t i = 0; i < personCount; i++) {
+    Sprites::drawSelfMasked(people[i].position.x  - camera.x, people[i].position.y  - camera.y, personSprite, personFrame);
   }
 }
 
-void drawRock() {
-  for (uint8_t i = 0; i < resourceMax; i++) {
-    Sprites::drawSelfMasked(rock[i].position.x  - camera.x, rock[i].position.y  - camera.y, rockSprite, 0);
+void animatePerson() {
+  if (personFrame < 2) {
+    ++personFrame;
+  }
+  else
+    personFrame = 0;
+}
+
+void personWalk() {
+  if (timer.currentMillis - timer.previousMillis >= 500) {
+    for (uint8_t i = 0; i < personCount; i++) {
+      walk = random(0, 4);
+      switch ( walk ) {
+        case 0:
+          ++people[i].position.x;
+          break;
+        case 1:
+          --people[i].position.x;
+          break;
+        case 2:
+          ++people[i].position.y;
+          break;
+        case 3:
+          --people[i].position.y;
+          break;
+      }
+      animatePerson();
+      timer.previousMillis = timer.currentMillis;
+    }
   }
 }
 
-void drawTree() {
-  for (uint8_t i = 0; i < resourceMax; i++) {
-    Sprites::drawSelfMasked(tree[i].position.x  - camera.x, tree[i].position.y  - camera.y, treeSprite, 0);
+void personSelection() {
+  for (uint8_t i = 0; i < personCount; i++) {
+    if (people[i].state == PersonState::notSelected && arduboy.collide(people[i].position, rectangle)){
+      people[i].state = PersonState::selected;
+      if (people[i].state == PersonState::selected) {
+        ++personSelect;
+      }
+    }
+    if (arduboy.pressed(A_BUTTON) && arduboy.pressed(B_BUTTON)) {
+      people[i].state = PersonState::notSelected;
+      personSelect = 0;
+    }
   }
 }
 
+void addPersonAt(uint16_t x, uint16_t y)
+{ if (arduboy.justPressed(B_BUTTON)) {
+    // Avoid trying to add more than the maximum
+    if (personCount < personMax)
+    {
+      // Person goes on the end of the list
+      people[personCount].position = {x, y};
+      ++personCount;
+    }
+  }
+}
 
-void resourceDraw(){
-  drawRock();
-  drawTree();
+void drawDebugInfo()
+{
+  arduboy.setCursor(0, 0);
+  arduboy.print(rectangle.x);
+
+  arduboy.setCursor(20, 0);
+  arduboy.print(rectangle.y);
+
+  arduboy.setCursor(0, 20);
+  arduboy.print("");
+
+  arduboy.setCursor(20, 20);
+  arduboy.print("");
+
+  arduboy.setCursor(0, 40);
+  arduboy.print(people[1].position.x);
+
+  arduboy.setCursor(20, 40);
+  arduboy.print(people[1].position.y);
 }
