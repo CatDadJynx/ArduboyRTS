fb2bd2a53d8a44745c32cd1b50a8ef3859b355fb
CatDadJynx <Andrew.crawford.440@gmail.com>
2019-05-12 22:03:48 -0400

    Update camera.h

--- camera.h
+++ camera.h
@@ -7,15 +7,15 @@ PointPos camera;
 
 void cameraInput(){
   if(arduboy.pressed(UP_BUTTON) && playerCursor.y <= 4) {
-    camera.y += 1;
-  }
-  if(arduboy.pressed(DOWN_BUTTON) && playerCursor.y >= 60) {
     camera.y -= 1;
   }
+  if(arduboy.pressed(DOWN_BUTTON) && playerCursor.y >= 60) {
+    camera.y += 1;
+  }
   if(arduboy.pressed(LEFT_BUTTON) && playerCursor.x <= 4) {
-    camera.x += 1;
+    camera.x -= 1;
   }
   if(arduboy.pressed(RIGHT_BUTTON) && playerCursor.x >= 124) {
-    camera.x -= 1;
+    camera.x += 1;
   }
 }
