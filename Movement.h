70632fda6671109e35042762501d09ca0a228525
CatDadJynx <Andrew.crawford.440@gmail.com>
2019-05-15 00:09:29 -0400

    Update Movement.h

--- Movement.h
+++ Movement.h
@@ -1,18 +1,20 @@
 // Distance
-int distanceSquared(int x0, int y0, int x1, int y1)
+int distanceSquared(float x0, float y0, float x1, float y1)
 {
-  const int xDistance = (x1 - x0);
-  const int yDistance = (y1 - y0);
+  const float xDistance = (x1 - x0);
+  const float yDistance = (y1 - y0);
   return ((xDistance * xDistance) + (yDistance * yDistance));
 }
 
 // Vector Magnitude
-float distance(int x0, int y0, int x1, int y1)
+float distance(float x0, float y0, float x1, float y1)
 {
   return sqrt(distanceSquared(x0, y0, x1, y1));
 }
 
 PointF vectorBetween(PointF point0, PointF point1)
 {
-  return { (point1.x - point0.x), (point1.y - point0.y) };
+  return { ((point1.x - point0.x)/distance(point0.x,point0.y,point1.x,point1.y)), ((point1.y - point0.y)/distance(point0.x,point0.y,point1.x,point1.y))};
 }
+
+
